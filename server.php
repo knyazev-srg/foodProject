428569067043453cae011424403ff17d61dad42c
Sergei <knyazev.code@gmail.com>
2023-05-21 16:51:15 +0300

    rewrote post function on fetch

--- server.php
+++ server.php
@@ -1,3 +1,3 @@
 <?php
-// $_POST = json_decode( file_get_contents("php://input"), true ); // JSON format
+$_POST = json_decode( file_get_contents("php://input"), true ); // JSON format
 echo var_dump($_POST);
