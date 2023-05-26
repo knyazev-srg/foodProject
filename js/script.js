45ec429a3884aa503e312a35a49b198a458f4bba
Sergei <knyazev.code@gmail.com>
2023-05-26 23:45:36 +0300

    added RegExp for carousel

--- js/script.js
+++ js/script.js
@@ -320,10 +320,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     next.addEventListener('click', () => {
-        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+        if (offset == (+width.replace(/\D/g, '') * (slides.length - 1))) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2); 
+            offset += +width.replace(/\D/g, ''); 
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -346,9 +346,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = +width.(/\D/g, '') * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= +width.(/\D/g, '');
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -374,7 +374,7 @@ window.addEventListener('DOMContentLoaded', () => {
           const slideTo = e.target.getAttribute('data-slide-to');
 
           slideIndex = slideTo;
-          offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+          offset = +width.replace(/\D/g, '') * (slideTo - 1);
 
           slidesField.style.transform = `translateX(-${offset}px)`;
 
